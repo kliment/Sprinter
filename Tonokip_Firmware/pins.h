94d38295100819f552ac89eff4012d1670d2273a
johnnyr <thehdesk@gmail.com>
2010-08-11 20:52:27 -0700

    changed arduino mega pins for ramps

--- Tonokip_Firmware/pins.h
+++ Tonokip_Firmware/pins.h
@@ -197,35 +197,35 @@
 #error Oops!  Make sure you have 'Arduino Mega' selected from the 'Tools -> Boards' menu.
 #endif
 
-#define X_STEP_PIN         22
-#define X_DIR_PIN          23
+#define X_STEP_PIN         26
+#define X_DIR_PIN          28
 #define X_ENABLE_PIN       24
-#define X_MIN_PIN           2
-#define X_MAX_PIN           3
+#define X_MIN_PIN           3
+#define X_MAX_PIN           2
 
-#define Y_STEP_PIN         25
-#define Y_DIR_PIN          26
-#define Y_ENABLE_PIN       27
-#define Y_MIN_PIN          18
-#define Y_MAX_PIN          19
+#define Y_STEP_PIN         38
+#define Y_DIR_PIN          40
+#define Y_ENABLE_PIN       36
+#define Y_MIN_PIN          16
+#define Y_MAX_PIN          17
 
-#define Z_STEP_PIN         28
-#define Z_DIR_PIN          29
-#define Z_ENABLE_PIN       30
-#define Z_MIN_PIN          20
-#define Z_MAX_PIN          21
+#define Z_STEP_PIN         44
+#define Z_DIR_PIN          46
+#define Z_ENABLE_PIN       42
+#define Z_MIN_PIN          18
+#define Z_MAX_PIN          19
 
-#define E_STEP_PIN          4
-#define E_DIR_PIN          31
-#define E_ENABLE_PIN       -1
+#define E_STEP_PIN         32
+#define E_DIR_PIN          34
+#define E_ENABLE_PIN       30
 
-#define LED_PIN            -1
+#define LED_PIN            13
 #define FAN_PIN            -1
-#define PS_ON_PIN          10
+#define PS_ON_PIN          -1
 #define KILL_PIN           -1
 
-#define HEATER_0_PIN        6
-#define TEMP_0_PIN          0    // MUST USE ANALOG INPUT NUMBERING NOT DIGITAL OUTPUT NUMBERING!!!!!!!!!
+#define HEATER_0_PIN        12
+#define TEMP_0_PIN          2   // MUST USE ANALOG INPUT NUMBERING NOT DIGITAL OUTPUT NUMBERING!!!!!!!!!
 
 
 
