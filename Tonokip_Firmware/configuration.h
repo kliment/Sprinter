ae9e6f61529b7d49e752a88994bc93bbbb089820
Emanuele Caruso <emanuele.caruso@gmail.com>
2011-06-24 02:34:48 +0200

    Added comment in configuration.h explaining how to set axes steps per unit

--- Tonokip_Firmware/configuration.h
+++ Tonokip_Firmware/configuration.h
@@ -88,7 +88,7 @@ long max_travel_acceleration_units_per_sq_second[] = {500,500,50}; // X, Y, Z ma
 //Calibration variables
 const int NUM_AXIS = 4; // The axis order in all axis related arrays is X, Y, Z, E
 bool axis_relative_modes[] = {false, false, false, false};
-float axis_steps_per_unit[] = {80.376,80.376,3200/1.25,16};
+float axis_steps_per_unit[] = {80.376,80.376,3200/1.25,16}; // {X steps per unit, Y steps per unit, Z steps per unit, E steps per unit}
 //For SAE Prusa mendeel float z_steps_per_unit = should be 3200/1.411 for 5/16-18 rod and 3200/1.058 for 5/16-24
 //float axis_steps_per_unit[] = {10.047,10.047,833.398,0.706};
 float max_feedrate[] = {200000, 200000, 240, 500000}; //mmm, acceleration!
