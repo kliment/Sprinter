aae3bdd7578bc67930cf46d17b11ed4ba5756844
kliment <kliment.yanev@gmail.com>
2011-07-09 13:12:11 +0200

    Add fastio to enable/disable

--- Sprinter/Sprinter.h
+++ Sprinter/Sprinter.h
@@ -1,6 +1,7 @@
 // Tonokip RepRap firmware rewrite based off of Hydra-mmm firmware.
 // Licence: GPL
 #include <WProgram.h>
+#include "fastio.h"
 extern "C" void __cxa_pure_virtual();
 void __cxa_pure_virtual(){};
 void get_command();
@@ -35,29 +36,29 @@ int analog2tempu(int raw, const short table[][2], int numtemps, int source);
 #define analog2temp( c ) analog2tempu((c),temptable,NUMTEMPS,HEATERSOURCE)
 #define analog2tempBed( c ) analog2tempu((c),bedtemptable,BNUMTEMPS,BEDSOURCE)
 #if X_ENABLE_PIN > -1
-#define  enable_x() digitalWrite(X_ENABLE_PIN, X_ENABLE_ON)
-#define disable_x() digitalWrite(X_ENABLE_PIN,!X_ENABLE_ON)
+#define  enable_x() WRITE(X_ENABLE_PIN, X_ENABLE_ON)
+#define disable_x() WRITE(X_ENABLE_PIN,!X_ENABLE_ON)
 #else
 #define enable_x() ;
 #define disable_x() ;
 #endif
 #if Y_ENABLE_PIN > -1
-#define  enable_y() digitalWrite(Y_ENABLE_PIN, Y_ENABLE_ON)
-#define disable_y() digitalWrite(Y_ENABLE_PIN,!Y_ENABLE_ON)
+#define  enable_y() WRITE(Y_ENABLE_PIN, Y_ENABLE_ON)
+#define disable_y() WRITE(Y_ENABLE_PIN,!Y_ENABLE_ON)
 #else
 #define enable_y() ;
 #define disable_y() ;
 #endif
 #if Z_ENABLE_PIN > -1
-#define  enable_z() digitalWrite(Z_ENABLE_PIN, Z_ENABLE_ON)
-#define disable_z() digitalWrite(Z_ENABLE_PIN,!Z_ENABLE_ON)
+#define  enable_z() WRITE(Z_ENABLE_PIN, Z_ENABLE_ON)
+#define disable_z() WRITE(Z_ENABLE_PIN,!Z_ENABLE_ON)
 #else
 #define enable_z() ;
 #define disable_z() ;
 #endif
 #if E_ENABLE_PIN > -1
-#define  enable_e() digitalWrite(E_ENABLE_PIN, E_ENABLE_ON)
-#define disable_e() digitalWrite(E_ENABLE_PIN,!E_ENABLE_ON)
+#define  enable_e() WRITE(E_ENABLE_PIN, E_ENABLE_ON)
+#define disable_e() WRITE(E_ENABLE_PIN,!E_ENABLE_ON)
 #else
 #define enable_e() ;
 #define disable_e() ;
@@ -69,8 +70,6 @@ void ClearToSend();
 void get_coordinates();
 void prepare_move();
 void linear_move(unsigned long steps_remaining[]);
-void do_step_update_micros(int axis);
 void do_step(int axis);
-void do_step_update_micros(int axis);
 void kill(byte debug);
 
