7a3bdb7028204f5cdd11e981fe8ac148eb9ad260
Changwoo Ryu <cwryu@debian.org>
2011-08-09 01:49:26 +0900

    Support Sanguino/Sanguinololu with ATmega1284P

--- Sprinter/pins.h
+++ Sprinter/pins.h
@@ -98,7 +98,7 @@
 #if MOTHERBOARD == 1
 #define KNOWN_BOARD 1
 
-#ifndef __AVR_ATmega644P__
+#if !defined(__AVR_ATmega644P__) && !defined(__AVR_ATmega1284P__) 
 #error Oops!  Make sure you have 'Sanguino' selected from the 'Tools -> Boards' menu.
 #endif
 
@@ -148,7 +148,7 @@
 #if MOTHERBOARD == 2
 #define KNOWN_BOARD 1
 
-#ifndef __AVR_ATmega644P__
+#if !defined(__AVR_ATmega644P__) && !defined(__AVR_ATmega1284P__) 
 #error Oops!  Make sure you have 'Sanguino' selected from the 'Tools -> Boards' menu.
 #endif
 
@@ -376,7 +376,7 @@
 #if MOTHERBOARD == 5
 #define KNOWN_BOARD 1
 
-#ifndef __AVR_ATmega644P__
+#if !defined(__AVR_ATmega644P__) && !defined(__AVR_ATmega1284P__) 
     #error Oops!  Make sure you have 'Sanguino' selected from the 'Tools -> Boards' menu.
 #endif
 
@@ -435,7 +435,7 @@
 #endif
 #if MOTHERBOARD == 6
 #define KNOWN_BOARD 1
-#ifndef __AVR_ATmega644P__
+#if !defined(__AVR_ATmega644P__) && !defined(__AVR_ATmega1284P__) 
 #error Oops!  Make sure you have 'Sanguino' selected from the 'Tools -> Boards' menu.
 #endif
 
