492682f06a7821f8e1bcd2153676e371d0186ad8
Alessandro Ranellucci <aar@cpan.org>
2011-08-26 19:40:37 +0200

    Fixed configuration for new [XYZ]_ENDSTOP_INVERT options

--- Sprinter/Configuration.h
+++ Sprinter/Configuration.h
@@ -32,10 +32,11 @@ float axis_steps_per_unit[] = {80, 80, 3200/1.25,700};
 #define ENDSTOPPULLUPS 1 // Comment this out (using // at the start of the line) to disable the endstop pullup resistors
 // The pullups are needed if you directly connect a mechanical endswitch between the signal and ground pins.
 //If your axes are only moving in one direction, make sure the endstops are connected properly.
-//If your axes move in one direction ONLY when the endstops are triggered, set ENDSTOPS_INVERTING to true here
-cost bool X_ENDSTOP_INVERT = false;//set to true to invert the logic of the endstops
-cost bool Y_ENDSTOP_INVERT = false;//set to true to invert the logic of the endstops
-cost bool Z_ENDSTOP_INVERT = false;//set to true to invert the logic of the endstops
+//If your axes move in one direction ONLY when the endstops are triggered, set [XYZ]_ENDSTOP_INVERT to true here:
+const bool X_ENDSTOP_INVERT = false;
+const bool Y_ENDSTOP_INVERT = false;
+const bool Z_ENDSTOP_INVERT = false;
+
 // This determines the communication speed of the printer
 #define BAUDRATE 115200
 
