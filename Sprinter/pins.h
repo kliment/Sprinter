eb2dc852d0ab71c5adb22f0efecf44b59a8f823a
midopple <mdoppler@gmx.at>
2012-01-29 01:27:25 +0100

    Merge commit 'fa63b839c6cbf261f2655e8783bf4745cc9ca513' into SprinterV2
    Conflicts:
    	Sprinter/Configuration.h

--- Sprinter/pins.h
+++ Sprinter/pins.h
@@ -143,7 +143,6 @@
 
 #endif
 
-
 /****************************************************************************************
 * RepRap Motherboard  ****---NOOOOOO RS485/EXTRUDER CONTROLLER!!!!!!!!!!!!!!!!!---*******
 *
@@ -637,20 +636,20 @@
 #define X_STEP_PIN         15
 #define X_DIR_PIN          21
 #define X_MIN_PIN          18
-#define X_MAX_PIN          -2
+#define X_MAX_PIN           -2
 
 #define Y_STEP_PIN         22
 #define Y_DIR_PIN          23
 #define Y_MIN_PIN          19
 #define Y_MAX_PIN          -1
 
-#define Z_STEP_PIN          3
-#define Z_DIR_PIN           2
+#define Z_STEP_PIN         3
+#define Z_DIR_PIN          2
 #define Z_MIN_PIN          20
 #define Z_MAX_PIN          -1
 
-#define E_STEP_PIN          1
-#define E_DIR_PIN           0
+#define E_STEP_PIN         1
+#define E_DIR_PIN          0
 
 #define LED_PIN            -1
 
@@ -663,39 +662,38 @@
 #define HEATER_0_PIN       13 // (extruder)
 
 #ifdef SANGUINOLOLU_V_1_2
-  #define HEATER_1_PIN       12 // (bed)
-  #define X_ENABLE_PIN       14
-  #define Y_ENABLE_PIN       14
-  #define Z_ENABLE_PIN       26
-  #define E_ENABLE_PIN       14
+#define HEATER_1_PIN       12 // (bed)
+#define X_ENABLE_PIN       14
+#define Y_ENABLE_PIN       14
+#define Z_ENABLE_PIN       26
+#define E_ENABLE_PIN       14
 #else
-  #define HEATER_1_PIN       14  // (bed)
-  #define X_ENABLE_PIN       -1
-  #define Y_ENABLE_PIN       -1
-  #define Z_ENABLE_PIN       -1
-  #define E_ENABLE_PIN       -1
+#define HEATER_1_PIN       14  // (bed)
+#define X_ENABLE_PIN       -1
+#define Y_ENABLE_PIN       -1
+#define Z_ENABLE_PIN       -1
+#define E_ENABLE_PIN       -1
 #endif
 
 #define TEMP_0_PIN          7   // MUST USE ANALOG INPUT NUMBERING NOT DIGITAL OUTPUT NUMBERING!!!!!!!!! (pin 33 extruder)
 #define TEMP_1_PIN          6   // MUST USE ANALOG INPUT NUMBERING NOT DIGITAL OUTPUT NUMBERING!!!!!!!!! (pin 34 bed)
-#define SDPOWER            -1
-#define SDSS               31
+#define SDPOWER          -1
+#define SDSS          31
 
 #endif
-
 
 
 /****************************************************************************************
 * Gen7 pin assignment
 *
 ****************************************************************************************/
-
 #if MOTHERBOARD == 7
 #define KNOWN_BOARD 1
 
 #if !defined(__AVR_ATmega644P__) && !defined(__AVR_ATmega1284P__) && !defined(__AVR_ATmega644__)
-     #error Oops!  Make sure you have 'Sanguino' selected from the 'Tools -> Boards' menu.
+    #error Oops!  Make sure you have 'Sanguino' selected from the 'Tools -> Boards' menu.
 #endif
+
 //x axis pins
     #define X_STEP_PIN      19
     #define X_DIR_PIN       18
@@ -721,7 +719,7 @@
     #define E_STEP_PIN      28     
     #define E_DIR_PIN       27     
     #define E_ENABLE_PIN    -1     
-    #define TEMP_0_PIN       1    // Extruder 
+    #define TEMP_0_PIN      1    // Extruder 
     #define HEATER_0_PIN     4    // Extruder
     #define HEATER_1_PIN     3    // Bed
 
@@ -729,12 +727,11 @@
     #define SDPOWER         -1
     #define SDSS            -1
     #define LED_PIN         -1    
-    #define TEMP_1_PIN       2    //Bed   
+    #define TEMP_1_PIN      2    //Bed   
     #define FAN_PIN         -1    
     #define PS_ON_PIN       15    
 
 #endif
-
 
 /****************************************************************************************
 * Teensylu 0.7 pin assingments (ATMEGA90USB)
