bd10b8f6eef3676ca4dee3fc024bb9b64e192b63
lincomatic <lincomatic@hotmail.com>
2012-03-17 21:31:46 -0700

    Make fastio & Arduino pin numbering consistent for AT90USB128x

--- Sprinter/pins.h
+++ Sprinter/pins.h
@@ -696,39 +696,37 @@
 #define KNOWN_BOARD 1
 
 
-#define X_STEP_PIN          0  
-#define X_DIR_PIN           1  
-#define X_ENABLE_PIN       39 
-#define X_MIN_PIN          13 
+#define X_STEP_PIN         28
+#define X_DIR_PIN          29
+#define X_ENABLE_PIN       19
+#define X_MIN_PIN          25
 #define X_MAX_PIN          -1    
 
-#define Y_STEP_PIN          2  
-#define Y_DIR_PIN           3 
-#define Y_ENABLE_PIN       38 
-#define Y_MIN_PIN          14 
+#define Y_STEP_PIN         30
+#define Y_DIR_PIN          31
+#define Y_ENABLE_PIN       26
+#define Y_MIN_PIN           20
 #define Y_MAX_PIN          -1    
 
-#define Z_STEP_PIN          4
-#define Z_DIR_PIN           5 
-#define Z_ENABLE_PIN       23 
-#define Z_MIN_PIN          15 
+#define Z_STEP_PIN         32
+#define Z_DIR_PIN          33
+#define Z_ENABLE_PIN       17
+#define Z_MIN_PIN          27
 #define Z_MAX_PIN          -1    
 
-#define E_STEP_PIN          6  
-#define E_DIR_PIN           7 
-#define E_ENABLE_PIN       19 
+#define E_STEP_PIN         34
+#define E_DIR_PIN          35
+#define E_ENABLE_PIN       13
 
-
-
-#define HEATER_0_PIN       21  // Extruder
-#define HEATER_1_PIN       20  // Bed
-#define FAN_PIN            22  // Fan   
+#define HEATER_0_PIN       15  // Extruder
+#define HEATER_1_PIN       14  // Bed
+#define FAN_PIN            16  // Fan   
 
 #define TEMP_0_PIN          7  // Extruder
 #define TEMP_1_PIN          6  // Bed
 
 #define SDPOWER            -1
-#define SDSS                8
+#define SDSS                20
 #define LED_PIN            -1
 #define PS_ON_PIN          -1
 #define KILL_PIN           -1 
@@ -736,9 +734,9 @@
 
 #ifndef SDSUPPORT
 // these pins are defined in the SD library if building with SD support  
-  #define SCK_PIN           9 
-  #define MISO_PIN         11 
-  #define MOSI_PIN         10 
+  #define SCK_PIN          21
+  #define MISO_PIN         22 
+  #define MOSI_PIN         23 
 #endif
 
 #endif
@@ -753,48 +751,46 @@
 #define KNOWN_BOARD 1
 
 
-#define X_STEP_PIN          0
-#define X_DIR_PIN           1
-#define X_ENABLE_PIN       39
-#define X_MIN_PIN          35
+#define X_STEP_PIN         28
+#define X_DIR_PIN          29
+#define X_ENABLE_PIN       19
+#define X_MIN_PIN          47
 #define X_MAX_PIN          -1
 
-#define Y_STEP_PIN          2
-#define Y_DIR_PIN           3
-#define Y_ENABLE_PIN       38
-#define Y_MIN_PIN           8
+#define Y_STEP_PIN         30
+#define Y_DIR_PIN          31
+#define Y_ENABLE_PIN       18
+#define Y_MIN_PIN           20
 #define Y_MAX_PIN          -1
 
-#define Z_STEP_PIN          4
-#define Z_DIR_PIN           5
-#define Z_ENABLE_PIN       23
+#define Z_STEP_PIN         32
+#define Z_DIR_PIN          33
+#define Z_ENABLE_PIN       17
 #define Z_MIN_PIN          36
 #define Z_MAX_PIN          -1
 
-#define E_STEP_PIN          6
-#define E_DIR_PIN           7
-#define E_ENABLE_PIN       19
+#define E_STEP_PIN         34
+#define E_DIR_PIN          35
+#define E_ENABLE_PIN       13
 
-
-
-#define HEATER_0_PIN       21  // Extruder
-#define HEATER_1_PIN       20  // Bed
-#define FAN_PIN            22  // Fan
+#define HEATER_0_PIN       15  // Extruder
+#define HEATER_1_PIN       14  // Bed
+#define FAN_PIN            16  // Fan
 
 #define TEMP_0_PIN          1  // Extruder
 #define TEMP_1_PIN          0  // Bed
 
 #define SDPOWER            -1
-#define SDSS                26
+#define SDSS                2
 #define LED_PIN            -1
 #define PS_ON_PIN          -1
 #define KILL_PIN           -1
 
 #ifndef SDSUPPORT
 // these pins are defined in the SD library if building with SD support
-  #define SCK_PIN          9
-  #define MISO_PIN         11
-  #define MOSI_PIN         10
+  #define SCK_PIN          21
+  #define MISO_PIN         22
+  #define MOSI_PIN         23
 #endif
 
 #endif
